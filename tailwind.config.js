3bfaa29ea524e6c308006e0a0d96fbc5bb60b4a6
Mohammed Gouse <gpasha798@gmail.com>
2023-12-27 22:16:11 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '10px 10px black',
+      }
+    },
+  },
+  plugins: [],
+}
+
